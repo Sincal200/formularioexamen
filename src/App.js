5fc6bfebebc024664f27f1020064953d635a54b1
Sincal <sincaljulio200@gmail.com>
2024-05-04 15:47:57 -0600

    Carga Inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission here
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <br />
+          <input type="text" className='entrada'/>
+        </label>
+        <br />
+        <label>
+          Email:
+          <br />
+          <input type="email"className='entrada'/>
+        </label>
+        <br />
+        <label>
+          Password:
+          <br />
+          <input type="password" className='entrada'/>
+        </label>
+        <br />
+        <label>
+          Confirm Password:
+          <br />
+          <input type="password" className='entrada'/>
+        </label>
+        <br />
+        <label>
+          Birthdate:
+          <br />
+          <input type="date" className='entrada' />
+        </label>
+        <br />
+        <label>
+          Gender:
+          <br />
+          <select>
+            <option value="">Select</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </label>
+        <br />
+        <label>
+          <input type="checkbox" />
+          Accept Terms and Conditions
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </main>
   );
 }
+
 
 export default App;
